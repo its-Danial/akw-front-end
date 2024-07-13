e7d96706e23d964a4d33bc7658fc6208b6198612
its-Danial <itsmalikdanial@gmail.com>
2024-07-13 08:40:57 +0500

    updated style

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <NavBar />
-  <main className="bg-muted/40 p-8 md:gap-8 md:p-10 md:px-16">
+  <main className="min-h-[calc(100vh-64px)] bg-muted/40 p-8 md:gap-8 md:p-10 md:px-16">
     <slot />
   </main>
 </template>
