4d6d0afba65dfe38b4ec2ae15b86e9cf8959a378
its-Danial <itsmalikdanial@gmail.com>
2024-07-14 11:17:14 +0500

    added user actions, added auto login logic

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,29 +1,49 @@
+import authService from '@/services/authService'
+import type { User } from '@/types'
 import { defineStore } from 'pinia'
 
+const AUTH_DATA = 'auth'
+
+const DEFAULT_STATE: User = JSON.parse(localStorage.getItem(AUTH_DATA) as string) ?? {
+  user: {
+    id: 2,
+    email: '',
+    role: ''
+  },
+  permission: {
+    canView: 0,
+    canEdit: 0,
+    canDelete: 0,
+    canViewAll: 0
+  },
+  token: ''
+}
+
 export const useUserStore = defineStore('user', {
-  state: () => ({ userData: { username: '' }, token: '' }),
+  state: () => DEFAULT_STATE,
   getters: {
     isLoggedIn: (state) => !!state.token
   },
   actions: {
-    async registerUser(username: string, password: string) {
-      console.log('registerUser', username, password)
-      this.userData.username = username
-
-      this.token = '123'
+    async registerUser(email: string, password: string) {
+      const { user, permission, token } = (await authService.register({ email, password })).data
+      this.user = user
+      this.permission = permission
+      this.token = token
     },
-
-    async loginUser(username: string, password: string) {
-      console.log('loginUser', username, password)
-      this.userData.username = username
-
-      this.token = '123'
+    async loginUser(email: string, password: string) {
+      const data = (await authService.login({ email, password })).data
+      localStorage.setItem(AUTH_DATA, JSON.stringify(data))
+      this.user = data.user
+      this.permission = data.permission
+      this.token = data.token
     },
-    async signOutUser() {
-      console.log('signOsignOutUserut')
-      this.userData.username = ''
-
-      this.token = ''
+    async logoutUser() {
+      await authService.logout()
+      this.user = DEFAULT_STATE.user
+      this.permission = DEFAULT_STATE.permission
+      this.token = DEFAULT_STATE.token
+      localStorage.removeItem(AUTH_DATA)
     }
   }
 })
