6de5158d08bb3673909e78f5de481412289197f8
its-Danial <itsmalikdanial@gmail.com>
2024-07-14 11:30:40 +0500

    added actions, sort handling

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UserPosts from '@/components/posts/UserPosts.vue'
+import entriesService from '@/services/entriesService'
+import type { Entry } from '@/types'
+import { onBeforeMount, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const entries = ref<Entry[]>()
+
+const setEntries = async (order = 'desc') => {
+  entries.value = (await (await entriesService.getEntries({ order })).data).entries
+}
+
+onBeforeMount(async () => {
+  setEntries()
+})
+
+const handleDeleteClick = async (entryId: number) => {
+  entries.value = entries.value!.filter((entry) => entry.id != entryId)
+}
+const handleEditClick = (entryId: number) => {
+  router.push({ name: 'create-entry', query: { id: entryId } })
+}
+
+const handleSortOrderChange = (newOrder: string) => {
+  setEntries(newOrder)
+}
+</script>
 
 <template>
   <main>
-    <UserPosts />
+    <UserPosts
+      v-if="entries"
+      :entries
+      @click:delete="handleDeleteClick"
+      @click:edit="handleEditClick"
+      @change:sortOrder="handleSortOrderChange"
+    />
   </main>
 </template>
