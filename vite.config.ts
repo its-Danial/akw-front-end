b041e99130ed2a97b60d36054feb37a8239ee8f7
its-Danial <itsmalikdanial@gmail.com>
2024-07-14 12:15:00 +0500

    remove dev tool

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,12 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(),
     Components({
       resolvers: [PrimeVueResolver()]
     })
