48474268dbda0377e7736a2cc75eb964e3fba83e
its-Danial <itsmalikdanial@gmail.com>
2024-07-14 11:31:56 +0500

    handle case of editing

--- src/pages/CreateEntryPage.vue
+++ src/pages/CreateEntryPage.vue
@@ -1,26 +1,69 @@
 <script setup lang="ts">
-import type { ImageFile } from '@/types'
-import { computed, ref } from 'vue'
+import config from '@/config'
+import entriesService from '@/services/entriesService'
+import type { Entry, ImageFile } from '@/types'
+import { computed, onBeforeMount, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
-const name = ref()
+const name = ref('')
 const image = ref()
-const loading = ref()
+const loading = ref(false)
 const errorMsg = ref('')
 
+const existingImage = ref()
+
 const submitIsDisabled = computed(() => !name.value || !image.value)
+
+const router = useRouter()
+const {
+  query: { id: existingArticleId }
+} = useRoute()
+
+onBeforeMount(async () => {
+  if (!existingArticleId) return
+
+  const entry = (
+    await (
+      await entriesService.getEntries({ entryId: existingArticleId as string })
+    ).data
+  ).entries as Entry
+
+  name.value = entry.name
+  existingImage.value = { name: entry.name, imagePath: `${config.api.baseUrl}/${entry.imagePath}` }
+})
 
 const handleImageSelect = (file: ImageFile) => {
   image.value = file
 }
 
-const handleSubmit = () => {
+const handleSubmit = async () => {
   if (submitIsDisabled.value) {
     errorMsg.value = 'Please fill in the fields.'
     return
   }
-  console.log('submit', name.value, image.value)
+
+  try {
+    loading.value = true
+    const formData = new FormData()
+
+    formData.append('name', name.value)
+    formData.append('image', image.value)
+
+    if (existingArticleId) {
+      await entriesService.updateEntry(existingArticleId as string, formData)
+    } else {
+      await entriesService.createEntry(formData)
+    }
+
+    router.push({ name: 'home' })
+  } catch (error) {
+    console.log(error)
+  } finally {
+    loading.value = false
+  }
 }
 </script>
+
 <template>
   <Toast />
   <form @submit.prevent="handleSubmit">
@@ -40,12 +83,16 @@ const handleSubmit = () => {
           />
         </div>
 
-        <ImageUploader @submit="handleImageSelect" />
+        <ImageUploader
+          :existingImage
+          @submit="handleImageSelect"
+          @clearExistingImage="existingImage = null"
+        />
       </template>
       <template #footer>
         <p v-if="errorMsg" class="text-sm text-red-400">{{ errorMsg }}</p>
         <div class="flex items-center justify-end">
-          <Button :disabled="submitIsDisabled" label="Submit" type="submit" :loading />
+          <Button :disabled="submitIsDisabled" label="Submit" type="submit" :loading="loading" />
         </div>
       </template>
     </Card>
